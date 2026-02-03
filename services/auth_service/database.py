07dfd584cf02102a4ac3dfb735e4a7b141c1ad3f
timsidorin <timsidorin@gmail.com>
2026-02-03 19:22:27 +1000

    Ренейминг

--- services/auth_service/database.py
+++ services/auth_service/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import (
     AsyncSession,
 )
 from sqlalchemy.orm import DeclarativeBase, declared_attr
-from services.Auth_service.config import get_db_url
+from services.auth_service.config import get_db_url
 
 DATABASE_URL = get_db_url()
 engine = create_async_engine(DATABASE_URL)
